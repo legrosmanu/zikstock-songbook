374efc064f1253a52c3cc0abebeb6544c088e00e
Emmanuel LEGROS <emmanuel.legros@gmail.com>
2019-08-26 21:49:58 +0100

    restart from zero to do it well

--- src/run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from zik_resource import zik_resource_api
-
-# Create an instance of Flask
-app = Flask(__name__)
-app.register_blueprint(zik_resource_api)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
