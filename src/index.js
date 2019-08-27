377ac3e41223eeb1e53abed413d6ff868dc23fb9
Emmanuel LEGROS <emmanuel.legros@gmail.com>
2019-08-27 19:11:05 +0100

    keep it empty for now. Just the config.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('spot4zik is running');
-});
-
-const server = app.listen(80, () => {
-    console.log('spot4zik is running on port 80');
-  });
-  
-  module.exports = server;
