fc6977f0fa33fa4f222c4a58339f751829753f71
Emmanuel LEGROS <emmanuel.legros@gmail.com>
2019-08-31 09:23:14 +0200

    Init the app, including tests and docker

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const { createTerminus } = require('@godaddy/terminus');
+
+async function onHealthCheck() {
+    // Nothing for now
+}
+
+const terminusOptions = {
+    healthChecks: { '/health-check': onHealthCheck }
+};
+
+app.use(bodyParser.json()); // for parsing application/json
+
+const server = http.createServer(app);
+createTerminus(server, terminusOptions); // for the health checks
+server.listen(process.env.PORT || 3000);
+
+module.exports = { app, server };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,25 @@
-var assert = require('assert');
-const server = require('../src/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../src/index').app;
+let server = require('../src/index').server;
 
-describe('Start the app', function() {
-  describe('First test to write', function() {
-    it("should say it's ok", function() {
-      assert.ok(true);
+chai.use(chaiHttp);
+
+after(() => { server.close(); });
+
+describe('The health check', function () {
+  it("should exist", (done) => {
+    chai.request(server).get('/health-check').end((err, res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
     });
   });
+  it("should be ok", (done) => {
+    chai.request(server).get('/health-check').end((err, res) => {
+      let statusIsOk = { status : "ok" };
+      expect(res.body).to.eql(statusIsOk);
+      done();
+    });
+  })
 });
