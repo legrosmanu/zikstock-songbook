f2b32783fa92eb12b9e0fcd4a7556c975d7b53c3
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-08-23 14:58:56 +0200

    update tests of the API

--- src/main/api/zik-resource-routes.js
+++ src/main/api/zik-resource-routes.js
@@ -1,7 +1,32 @@
-module.exports = class ZikResourceRoutes {
+class ZikResourceRoutes {
 
     constructor(app) {
         this.app = app;
     }
 
-};
+    setRoutes() {
+        this.app.post('/api/zik-resources', this.postZikResource);
+        this.app.get('/api/zik-resources', this.getZikResources);
+        this.app.get('/api/zik-resources/:id', this.getZikResource);
+        this.app.delete('/api/zik-resources/:id', this.deleteZikResource);
+    }
+
+    async postZikResource(req, res) {
+        res.sendStatus(501);
+    }
+
+    async getZikResources(req, res) {
+        res.sendStatus(501);
+    }
+
+    async getZikResource(req, res) {
+        res.sendStatus(501);
+    }
+    
+    async deleteZikResource(req, res) {
+        res.sendStatus(501);
+    }
+
+}
+
+module.exports = ZikResourceRoutes;
