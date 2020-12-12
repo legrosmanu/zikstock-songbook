f2920b974b88f8718d1d26ea7a301542c3754232
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-12 17:47:25 +0100

    Fix unit tests on Travis

--- src/test/unit/zik-resource/zik-resource-api.test.js
+++ src/test/unit/zik-resource/zik-resource-api.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../../index');
+const app = require('../../../main/app');
 const ZikResourceDao = require('../../../main/zikresource/zikresource-dao');
 const ZikStockError = require('../../../main/zikresource/zikstock-error');
 
