b1b4d4ae58cb1492ef91a55a44b2ffebc2e8f81e
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-12 17:30:32 +0100

    Fix unit tests on Travis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const app = require('./main/app');
+const mongoose = require('mongoose');
+
+const connect = mongoose.connect('mongodb://localhost:27017/zikstock'); // TODO : change to use env var
+connect.then(() => {
+    console.log("Connected to MongoDB");
+}, (err) => { console.log(err); });
 
 const server = http.createServer(app);
 server.listen(process.env.NODE_PORT || 3000);
--- src/main/app.js
+++ src/main/app.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const connect = mongoose.connect('mongodb://localhost:27017/zikstock'); // TODO : change to use env var
-connect.then(() => {
-    console.log("Connected to MongoDB");
-}, (err) => { console.log(err); });
 
 const zikResourceRouter = require('./zikresource/zikresource-api');
 
