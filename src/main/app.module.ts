1ba7b0fc77f18dea64768a9e3caa49830aa12b87
Emmanuel Legros <e.legros@groupe-faubourg.com>
2021-05-16 11:06:52 +0200

    Revert "Start NetsJS migration"
    This reverts commit 1316010097ab185b5c2154907912f80266173bed.

--- src/main/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { ZikresourceModule } from './zikresource/zikresource.module';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [ZikresourceModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- src/main/zikresource/dto/tag.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class Tag {
-  @IsNotEmpty()
-  label: string;
-  @IsNotEmpty()
-  value: string;
-}
