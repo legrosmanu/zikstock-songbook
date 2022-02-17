e29a82c5cba6346d083862673df5aac7917224c1
Emmanuel Legros <e.legros@groupe-faubourg.com>
2022-02-17 09:15:37 +0100

    Adapt exception handler

--- src/main/java/com/zikstock/songbook/RestExceptionHandler.java
+++ src/main/java/com/zikstock/songbook/RestExceptionHandler.java
@@ -3,21 +3,23 @@ package com.zikstock.songbook;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
-    public ResponseEntity<ApiError> handleDataFormatError(RuntimeException ex, WebRequest request, HttpServletRequest httpRequest) {
-        var apiError = new ApiError(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase(), httpRequest.getRequestURI(), ex.getLocalizedMessage());
-        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String uri = ((ServletWebRequest)request).getRequest().getRequestURI();
+        var specificBody = new ApiError(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase(), uri, ex.getLocalizedMessage());
+
+        return super.handleExceptionInternal(ex, specificBody, headers, status, request);
     }
 
 }
--- src/main/java/com/zikstock/songbook/zikresource/Zikresource.java
+++ src/main/java/com/zikstock/songbook/zikresource/Zikresource.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class Zikresource {
 
     @Id
