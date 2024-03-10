0e95110a1320c175001882a05de0d15650de7154
Emmanuel Legros <emmanuel.legros@gmail.com>
2024-03-10 13:48:24 +0000

    Add eslint

--- src/util/api-error.ts
+++ src/util/api-error.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { logger } from './logger';
 
 export type ApiError = {
@@ -7,11 +7,12 @@ export type ApiError = {
     message: string;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isApiError = (error: any): error is ApiError => {
     return typeof error.status === 'number' && typeof error.code === 'string' && typeof error.message === 'string';
 };
 
-export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const errorMiddleware = (err: unknown, req: Request, res: Response) => {
     let status = 500;
     let error = null;
     if (err != null) {
